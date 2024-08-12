594eb265b85b0729640baad1f00db634d980f7d4
Ivan Goliukov <168529172+Laowai12@users.noreply.github.com>
2024-08-12 21:03:31 +1000

    Add files via upload

---
+++ Module3Hard.py
@@ -0,0 +1,27 @@
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])]
+
+def calculate_structure_sum(struct):
+    count = 0
+    for item in struct:
+        if isinstance(item, list | tuple | set):
+          count += calculate_structure_sum(item)
+        if isinstance(item, dict):
+          for i in item:
+            count += len(i)
+          count += sum(item.values())
+        if isinstance(item, int):
+          count += item
+        if isinstance(item, str):
+          count += len(item)
+
+    return count
+
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
